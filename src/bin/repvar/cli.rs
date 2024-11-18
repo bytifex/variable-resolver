13cb221d9293ed9ce90012dce87775602a3e7138
Gergo Roth <roth@bytifex.com>
2024-11-18 20:27:34 +0100

    removed some leftover unused code

--- src/bin/repvar/cli.rs
+++ src/bin/repvar/cli.rs
@@ -1,40 +1,4 @@
-use std::path::PathBuf;
-
 use clap::Parser;
-
-#[derive(Debug, Parser)]
-pub struct ServeParams {
-    #[arg(
-        short('l'),
-        long("listener-address"),
-        help("Address where the proxy server accepts the connections (e.g., 127.0.0.1:8000)")
-    )]
-    pub listener_address: String,
-}
-
-#[derive(Debug, Parser)]
-pub struct QueryParams {
-    #[arg(
-        short('q'),
-        long("query-path"),
-        help("Path to the query file (e.g., ./query.graphql)")
-    )]
-    pub query_path: PathBuf,
-
-    #[arg(
-        short('a'),
-        long("server-address"),
-        help("Address where the server accepts the connections (e.g., https://someserver/api/graphql)")
-    )]
-    pub server_address: String,
-}
-
-#[derive(Debug, Parser)]
-pub enum Command {
-    Serve(ServeParams),
-    Sdl,
-    Query(QueryParams),
-}
 
 #[derive(Debug, Parser)]
 pub struct Cli {
